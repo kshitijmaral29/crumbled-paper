df96cc00c308adb74652e94d42121b211dede7d6
kshitijmaral29 <76586291+kshitijmaral29@users.noreply.github.com>
2021-05-04 14:47:57 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,31 @@
+class Paper{
+    constructor(x,y,r){
+        var options ={
+            isStatic : false,
+            restitution : 0.3,
+            friction:0,
+            density:1.2
+        }
+        this.body = Bodies.circle(x,y,r/3,options);
+
+        this.image = loadImage("paper.png");
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        World.add(world,this.body);
+        
+        
+    }
+    display(){
+     
+        var pos = this.body.position;
+        push();
+      translate(pos.x,pos.y);
+      rectMode(CENTER);
+        imageMode(CENTER);
+        image(this.image,0,0,this.r,this.r);
+
+        pop();
+   
+    }
+}
